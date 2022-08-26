2f8a648e1e5d8c4756f9390163b3fa8cf0cd4a66
David Hevesi <davidhevesi2008@gmail.com>
2022-08-26 16:26:16 -0400

    add refresh functinionality to button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-import Loading from "./Loading";
-import Tours from "./Tours";
-const url = "https://course-api.com/react-tours-project";
+import Loading from './Loading'
+import Tours from './Tours'
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
 
   const removeTours = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id);
-    setTours(newTours);
-  };
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
   const fetchTours = async () => {
-    setLoading(true);
+    setLoading(true)
     try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      setLoading(false);
-      setTours(tours);
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
     } catch (error) {
-      setLoading(false);
-      console.log(error);
+      setLoading(false)
+      console.log(error)
     }
-  };
+  }
   useEffect(() => {
-    fetchTours();
-  }, []);
+    fetchTours()
+  }, [])
   if (loading) {
     return (
       <main className="flex justify-center">
         <Loading />
       </main>
-    );
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <main className="flex justify-center pt-20 items-center">
+        <div className="">
+          <h2 className="text-2xl font-semibold">..No tours</h2>
+          <button
+            className="bg-amber-500  py-2 px-6 rounded-2xl"
+            onClick={fetchTours}
+          >
+            {' '}
+            Refresh
+          </button>
+        </div>
+      </main>
+    )
   }
   return (
     <main className="container mx-auto max-w-md">
@@ -40,7 +56,7 @@ function App() {
         <Tours tours={tours} removeTour={removeTours} />
       </div>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
